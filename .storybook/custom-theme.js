08f88daa0d239089a786c3ff1b8353304a403ddc
River <ahe794613@gmail.com>
2022-10-23 20:17:52 +0800

    [INIT] Configure nextJS, typescript, storybook, tailwind

---
+++ .storybook/custom-theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+// import logo from "./logo.png";
+
+export default create({
+    base: "light",
+    brandTitle: "Task board",
+    // brandUrl: 'https://example.com',
+    // brandImage: logo,
+});
---
+++ .storybook/preview.js
@@ -0,0 +1,47 @@
+import '../styles/globals.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  backgrounds: {
+    default: 'base',
+    values: [
+      {
+        name: 'base',
+        value: '#f5f5f5',
+      },
+      {
+        name: 'light',
+        value: '#ffffff',
+      },
+      {
+        name: 'dark',
+        value: 'rgb(51, 51, 51)',
+      },
+    ],
+  },
+
+  options: {
+    storySort: {
+      includeName: true,
+      method: '',
+      order: ['atoms', 'molecules', 'organisms', 'templates', 'pages'],
+    },
+  },
+}
+
+// Add support for Next.js `NextImage` component feature
+import * as NextImage from 'next/image'
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
